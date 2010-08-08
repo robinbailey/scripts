c06d3916a91194fec75680d69c439846b675c8ad
robinbailey <robinbailey86@googlemail.com>
2010-08-08 21:37:26 +0100

    Added folder scanner

---
+++ folderscan.pl
@@ -0,0 +1,90 @@
+#!/usr/bin/perl
+
+#Usage : folderscan.pl <hostname>
+
+use LWP::Simple;
+unlink("folders-found.txt");
+$host = $ARGV[0];
+$hostname = $host;
+
+open(FILE, "folder-list.txt") or die("Can not open file.\n"); 
+@lines = <FILE>; 
+close(FILE); 
+
+$robots = get("http://".$host."/robots.txt");
+@robots = split(/\n/, $robots);
+foreach $robot (@robots)
+{
+	if (($robot =~ m/Disallow: \/([a-zA-Z0-9\/\?\.\-_]+)/) || ($robot =~ m/Allow: \/([a-zA-Z0-9\/\?\.\-_]+)/))
+	{
+		$line = $1;
+		$line =~ s/\?$//;
+		$line =~ s/\/$//;
+		if (($line) && ($line !=~ /^\?$/) && ($line !=~ /^\/$/))
+		{
+			unshift (@lines, $line);
+		}
+	}
+}
+
+
+system("CLS");
+&folderscan;
+
+
+
+#############################################################################
+#First scan has taken place                                                 #
+#Any that were found are stored in @working                                 #
+#############################################################################
+foreach (@working)
+{
+	$recursive = $_;
+	$host = $hostname;
+	$x++;
+	&folderscan;
+}
+print "\n$x folders found\n";
+
+
+
+sub folderscan
+{
+
+$content = getstore("http://".$host."/sadlfhw94evnetyvoesnmvtoescmn47vob", "/dev/null");
+if ($content != 404)
+{
+	print "Error, site didn't return 404\n";
+	exit;
+}
+
+
+	foreach (@lines)
+	{
+		$folder = $_;
+		chomp ($folder);
+		$content = getstore("http://".$host.$recursive."/".$folder, NUL);
+		if ($content == 404)
+		{
+			next;
+		}
+		
+		elsif ($content == 200)
+		{
+			$content = $host.$recursive."/".$folder;
+			$folder = "/$folder";
+			print "$content\n";
+			push(@working, $recursive.$folder);
+			$num++;
+			open(FILE, ">>folders-found.txt"); 
+			print FILE "$content\n"; 
+			close(FILE);
+			
+		}
+		else
+		{
+			$content .= " ".$recursive."/".$folder."\n";
+			print $content;
+		}
+	}
+}
