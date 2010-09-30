3f7179baa7029c092865f7021a34961ad1bbe73e
user <user@arch.localdomain>
2010-09-30 22:47:43 +0100

    Cleanup up, added comments

--- folderscan.pl
+++ folderscan.pl
@@ -1,8 +1,8 @@
 #!/usr/bin/perl
+use LWP::Simple;
+
 
 #Usage : folderscan.pl <hostname>
-
-use LWP::Simple;
 unlink("folders-found.txt");
 $host = $ARGV[0];
 $hostname = $host;
@@ -11,6 +11,7 @@ open(FILE, "folder-list.txt") or die("Can not open file.\n");
 @lines = <FILE>; 
 close(FILE); 
 
+# Scan any folders in the robots.txt file on the site
 $robots = get("http://".$host."/robots.txt");
 @robots = split(/\n/, $robots);
 foreach $robot (@robots)
@@ -27,16 +28,11 @@ foreach $robot (@robots)
 	}
 }
 
-
-system("CLS");
+system("clear");
 &folderscan;
 
-
-
-#############################################################################
-#First scan has taken place                                                 #
-#Any that were found are stored in @working                                 #
-#############################################################################
+# First scan has taken place
+# Any that were found are stored in @working
 foreach (@working)
 {
 	$recursive = $_;
@@ -46,19 +42,19 @@ foreach (@working)
 }
 print "\n$x folders found\n";
 
-
+#
+# Main program logic
+#
 
 sub folderscan
 {
 
-$content = getstore("http://".$host."/sadlfhw94evnetyvoesnmvtoescmn47vob", "/dev/null");
-if ($content != 404)
-{
-	print "Error, site didn't return 404\n";
-	exit;
-}
-
-
+	$content = getstore("http://".$host."/sadlfhw94evnetyvoesnmvtoescmn47vob", "/dev/null");
+	if ($content != 404)
+	{
+		print "Error, site didn't return 404\n";
+		exit;
+	}
 	foreach (@lines)
 	{
 		$folder = $_;
@@ -68,7 +64,6 @@ if ($content != 404)
 		{
 			next;
 		}
-		
 		elsif ($content == 200)
 		{
 			$content = $host.$recursive."/".$folder;
