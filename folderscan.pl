20ad491c2e256d85e17b1338deafc2b9afd4bb7c
robinbailey <robinbailey86@googlemail.com>
2010-10-18 16:50:57 +0100

    Workaround for site not returning 404

--- folderscan.pl
+++ folderscan.pl
@@ -1,11 +1,15 @@
 #!/usr/bin/perl
 use LWP::Simple;
 
-
 #Usage : folderscan.pl <hostname>
 unlink("folders-found.txt");
 $host = $ARGV[0];
 $hostname = $host;
+
+# Number of characters different pages can be while being classified as the same
+# If you're getting false postives, raise this to above the diff printed
+$maxdiff = 50;
+
 
 open(FILE, "folder-list.txt") or die("Can not open file.\n"); 
 @lines = <FILE>; 
@@ -52,34 +56,67 @@ sub folderscan
 	$content = getstore("http://".$host."/sadlfhw94evnetyvoesnmvtoescmn47vob", "/dev/null");
 	if ($content != 404)
 	{
-		print "Error, site didn't return 404\n";
-		exit;
+		print "Site didn't return 404\nFalling back to length based page matching\nIf you get too many false positives, raise \$maxlength to above the diff printed for the false positives";
+		$content = get("http://".$host."/sadlfhw94evnetyvoesnmvtoescmn47vob");
+		$len = length($content);
 	}
-	foreach (@lines)
+	if ($len)	# Site didn't return a 404, so we have to check by page length
 	{
-		$folder = $_;
-		chomp ($folder);
-		$content = getstore("http://".$host.$recursive."/".$folder, NUL);
-		if ($content == 404)
+		foreach (@lines)
 		{
-			next;
-		}
-		elsif ($content == 200)
-		{
-			$content = $host.$recursive."/".$folder;
-			$folder = "/$folder";
-			print "$content\n";
-			push(@working, $recursive.$folder);
-			$num++;
-			open(FILE, ">>folders-found.txt"); 
-			print FILE "$content\n"; 
-			close(FILE);
-			
-		}
-		else
-		{
-			$content .= " ".$recursive."/".$folder."\n";
-			print $content;
+			$folder = $_;
+			chomp ($folder);
+			$content = get("http://".$host.$recursive."/".$folder);
+			$newlen = length($content);
+			$diff = abs(($len - $newlen));
+			if ($diff <= $maxdiff)
+			{
+				next;
+			}
+			else
+			{
+				$content = $host.$recursive."/".$folder;
+				$folder = "/$folder";
+				print "$content";
+			 	print "\t\t\t\t\t$diff\n";
+				push(@working, $recursive.$folder);
+				$num++;
+				open(FILE, ">>folders-found.txt"); 
+				print FILE "$content\n"; 
+				close(FILE);
+				
+			}
 		}
 	}
+	else
+	{
+		foreach (@lines)
+		{
+			$folder = $_;
+			chomp ($folder);
+			$content = getstore("http://".$host.$recursive."/".$folder, NUL);
+			if ($content == 404)
+			{
+				next;
+			}
+			elsif ($content == 200)
+			{
+				$content = $host.$recursive."/".$folder;
+				$folder = "/$folder";
+				print "$content\n";
+				push(@working, $recursive.$folder);
+				$num++;
+				open(FILE, ">>folders-found.txt"); 
+				print FILE "$content\n"; 
+				close(FILE);
+				
+			}
+			else
+			{
+				$content .= " ".$recursive."/".$folder."\n";
+				print $content;
+			}
+		}
+	}
+
 }
